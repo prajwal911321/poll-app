1b147b3a0e8e2136c80d45013bab99675bfd4b01
Prajwal H T <prajwaltgowda20@gmail.com>
2023-05-31 20:01:25 +0530

    V1.02

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,6 +76,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+""""
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME' : 'postgres',
         'USER' : 'postgres',
@@ -84,7 +92,7 @@ DATABASES = {
         'PORT' : '5432',
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
